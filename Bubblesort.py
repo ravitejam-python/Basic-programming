4f89c4056dfff188e178d62f71e57598bd18df74
ravitejam-python <ravitejamusinuri1997@gmail.com>
2020-10-12 11:40:48 +0530

    added code for sorting the list in descending order

--- Bubblesort.py
+++ Bubblesort.py
@@ -11,6 +11,16 @@ def sort(list):
                 list[i]=list[j]
                 list[j]=temp
 
+def sort_desc(list):
+
+    for i in range(0,len(list)):
+        for j in range(i+1, len(list)):
+
+            if list[i]<list[j]:
+                temp=list[i]
+                list[i]=list[j]
+                list[j]=temp
+
 
 
 list= [1,5,8,9,2,4]
@@ -19,6 +29,12 @@ print(list)                        #Printing the list before sorting
 
 #print(sorted(list))                # Inbuilt function to sort the list/array
 
-sort(list)
 
+sort(list)
 print(list)                        #Printing the list after sorting
+
+
+#Bubble sort is all about sorting the array in descending order
+
+sort_desc(list)
+print(list)
